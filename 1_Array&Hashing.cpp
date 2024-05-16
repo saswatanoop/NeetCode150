d3d468689677381f9b1c96d06701e97ec0485c48
sswt19 <saswatanoop@gmail.com>
2024-05-16 13:41:32 +0530

    solved group Anagrams

--- 1_Array&Hashing.cpp
+++ 1_Array&Hashing.cpp
@@ -16,6 +16,7 @@ using namespace std;
 1. Contains Duplicate :https://leetcode.com/problems/contains-duplicate/description/
 2. Valid Anagram: https://leetcode.com/problems/valid-anagram/description/
 3. Two Sum: https://leetcode.com/problems/two-sum/description/
+4. Group Anagrams: https://leetcode.com/problems/group-anagrams/description/
 */
 
 // 1.
@@ -94,5 +95,66 @@ public:
             value_to_index[nums[i]] = i;
         }
         return {};
+    }
+};
+
+// 4.
+class GroupAnagrams
+{
+    /*
+        Number of strings: n, max size of each string: m
+        1. Sort the strings and anagrams will be same after being sorted
+            T:O(n*mlogm) S:O(nk)
+
+        2. Do counting sort, saswat=> a2s2t1w1, anagrams will have same freq mapping
+            T:O(nm) S:O(nm)
+    */
+private:
+    string createFreqMap(const string &s)
+    {
+        vector<int> freq(26, 0);
+        for (auto c : s)
+            freq[c - 'a']++;
+
+        string freqMap = "";
+        for (int i = 0; i < 26; i++)
+            if (freq[i] > 0)
+                freqMap += char('a' + i) + to_string(freq[i]);
+        return freqMap;
+    }
+
+public:
+    // 1
+    vector<vector<string>> groupAnagramsSortMethod(vector<string> &strs)
+    {
+        unordered_map<string, vector<string>> groups;
+
+        for (int i = 0; i < strs.size(); i++)
+        {
+            string copy(strs[i]);
+            sort(copy.begin(), copy.end());
+            if (groups.find(copy) == groups.end())
+                groups[copy] = {};
+            groups[copy].push_back(strs[i]);
+        }
+        vector<vector<string>> ans;
+        for (auto p : groups)
+            ans.push_back(p.second);
+
+        return ans;
+    }
+    // 2
+    vector<vector<string>> groupAnagrams(vector<string> &strs)
+    {
+        vector<vector<string>> ans;
+        unordered_map<string, vector<string>> groupedAnagrams;
+        for (auto s : strs)
+        {
+            auto freqMap = createFreqMap(s);
+            groupedAnagrams[freqMap].push_back(s);
+        }
+        for (auto p : groupedAnagrams)
+            ans.push_back(p.second);
+        return ans;
     }
 };
