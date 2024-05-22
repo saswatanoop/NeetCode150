248a01994eec012daa171c48480a7090ca0fb981
sswt19 <saswatanoop@gmail.com>
2024-05-22 16:51:55 +0530

    solved Min stack

--- 4_Stack.cpp
+++ 4_Stack.cpp
@@ -14,9 +14,11 @@ using namespace std;
 
 /*
 1. Valid Parentheses: https://leetcode.com/problems/valid-parentheses/description/
-2.
+2. Min Stack: https://leetcode.com/problems/min-stack/description/
+3.
 */
 
+// 1.
 class ValidParantheses
 {
 public:
@@ -40,5 +42,72 @@ public:
         }
         // don't miss below point if stack is empty or not
         return st.empty();
+    }
+};
+
+// 2.
+class MinStackUsingOneStack
+{
+    stack<pair<int, int>> s;
+
+public:
+    MinStackUsingOneStack()
+    {
+    }
+
+    void push(int val)
+    {
+        int minVal = s.empty() ? val : min(s.top().second, val);
+        s.push({val, minVal});
+    }
+
+    void pop()
+    {
+        s.pop();
+    }
+
+    int top()
+    {
+        return s.top().first;
+    }
+
+    int getMin()
+    {
+        return s.top().second;
+    }
+};
+class MinStackUsingTwoStacks
+{
+public:
+    stack<int> s;  // normal stack
+    stack<int> ms; // min stack
+    MinStackUsingTwoStacks()
+    {
+    }
+
+    void push(int val)
+    {
+        s.push(val);
+        // if the item pushed is less than or equal to the min till now push it
+        if (ms.empty() || val <= ms.top())
+            ms.push(val);
+    }
+
+    void pop()
+    {
+        // pop from min top if the stack top is same as min-top
+        if (s.top() == ms.top())
+            ms.pop();
+        s.pop();
+    }
+
+    int top()
+    {
+        return s.top();
+    }
+
+    int getMin()
+    {
+        return ms.top();
     }
 };
