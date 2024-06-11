d6e835ee0b15246e1c0e6c9e50c1af5ace3e2e7b
sswt19 <saswatanoop@gmail.com>
2024-06-11 21:24:21 +0530

    solved Word Search II

--- 10_Tries.cpp
+++ 10_Tries.cpp
@@ -16,6 +16,7 @@ using namespace std;
 /*
 1. Implement Trie (Prefix Tree): https://leetcode.com/problems/implement-trie-prefix-tree/description/
 2. Design Add and Search Words Data Structure: https://leetcode.com/problems/design-add-and-search-words-data-structure/
+3. Word Search II: https://leetcode.com/problems/word-search-ii/description/
 */
 
 // 1
@@ -119,6 +120,77 @@ public:
     bool search(string word)
     {
         return t.serach_with_dot_in_string(word);
+    }
+};
+
+// 3
+class WordSearch2
+{
+    TrieNode *head = new TrieNode();
+
+    void insert(string word)
+    {
+        auto cur = head;
+        for (auto c : word)
+        {
+            if (cur->children.find(c) == cur->children.end())
+                cur->children[c] = new TrieNode();
+            cur = cur->children[c];
+        }
+        cur->isEnd = true;
+    }
+
+public:
+    // we will use backtracking as in word search and use trie to search for all words
+    void find_words_in_grid(vector<vector<char>> &board, int i, int j, string &ans, TrieNode *cur, vector<string> &all_words)
+    {
+        if (i >= board.size() || j >= board[i].size() || !cur || board[i][j] == ' ')
+            return;
+
+        if (cur->children.find(board[i][j]) == cur->children.end())
+            return;
+
+        // mark the i,j as visited, go one level deeper in trie node and add c to ans string
+        char c = board[i][j];
+        board[i][j] = ' ';
+        cur = cur->children[c];
+        ans += c;
+
+        // if the node we are at in trie is end of a word push it to list.
+        if (cur->isEnd)
+        {
+            all_words.push_back(ans);
+            cur->isEnd = false; // mark false so that same word is not pushed again, we want unique list
+        }
+
+        // go in all 4 directions
+        int dx[4] = {-1, 1, 0, 0};
+        int dy[4] = {0, 0, -1, 1};
+        for (int k = 0; k < 4; k++)
+        {
+            int x = i + dx[k];
+            int y = j + dy[k];
+            find_words_in_grid(board, x, y, ans, cur, all_words);
+        }
+        // unmark the board and remove the character added for index i,j
+        board[i][j] = c;
+        ans.pop_back();
+    }
+    vector<string> findWords(vector<vector<char>> &board, vector<string> &words)
+    {
+        // insert in all words we want to search in trie
+        for (auto word : words)
+            insert(word);
+
+        vector<string> all_words;
+        string word = "";
+
+        // use each i,j as starting for a word
+        for (int i = 0; i < board.size(); i++)
+            for (int j = 0; j < board[0].size(); j++)
+                find_words_in_grid(board, i, j, word, head, all_words);
+
+        return all_words;
     }
 };
 //
