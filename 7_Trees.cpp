074a02aad7dc17136711b242b8f8741cd0981f0e
sswt19 <saswatanoop@gmail.com>
2024-07-08 16:11:36 +0530

    solved Balanced Binary Tree

--- 7_Trees.cpp
+++ 7_Trees.cpp
@@ -22,10 +22,21 @@ struct TreeNode
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
                                                        right(right) {}
 };
+
 /*
+preorder and order traversal with NULL values will always be unique for each tree
+
+Binary Tree:
 1. Invert Binary Tree/Convert a Binary Tree into its Mirror Tree: https://leetcode.com/problems/invert-binary-tree/description/
 2. Maximum-Depth/Height of Binary Tree: https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
 3. Diameter of Binary Tree: https://leetcode.com/problems/diameter-of-binary-tree/description/
+4. Balanced Binary Tree: https://leetcode.com/problems/balanced-binary-tree/description/
+5. Same Tree: https://leetcode.com/problems/same-tree/description/
+6. Subtree of Another Tree: https://leetcode.com/problems/subtree-of-another-tree/description/
+
+
+Binary Search Tree:
+
 */
 
 // ============================================= Binary Tree ==============================================
@@ -107,4 +118,38 @@ public:
         return ans == 0 ? 0 : ans - 1;
     }
 };
-// ============================================= Binary Search Tree ==============================================
+
+// 4
+class IsBalancedBT
+{
+    /*
+        We will check if the tree is balanced in each node with bottom up approach, and return height of the subtree
+        T: O(n)
+        S: O(h)=>O(n) worst case
+    */
+    int isBalanced_helper(TreeNode *root, bool &balanced)
+    {
+        if (!root)
+            return 0;
+
+        int left_height = isBalanced_helper(root->left, balanced);
+        int right_height = isBalanced_helper(root->right, balanced);
+
+        if (abs(left_height - right_height) > 1)
+            balanced = false;
+
+        return 1 + max(left_height, right_height);
+    }
+
+public:
+    bool isBalanced(TreeNode *root)
+    {
+        // all subtrees should be balanced as well
+        bool balanced = true;
+        isBalanced_helper(root, balanced);
+        return balanced;
+    }
+};
+//  ============================================= Binary Search Tree ==============================================
+
+//
