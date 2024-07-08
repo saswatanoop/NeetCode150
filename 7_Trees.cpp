2cf28ea16755b9f8b65f93749d791a1d6f145408
sswt19 <saswatanoop@gmail.com>
2024-07-08 12:40:27 +0530

    solved Diameter of Binary Tree

--- 7_Trees.cpp
+++ 7_Trees.cpp
@@ -25,6 +25,7 @@ struct TreeNode
 /*
 1. Invert Binary Tree/Convert a Binary Tree into its Mirror Tree: https://leetcode.com/problems/invert-binary-tree/description/
 2. Maximum-Depth/Height of Binary Tree: https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
+3. Diameter of Binary Tree: https://leetcode.com/problems/diameter-of-binary-tree/description/
 */
 
 // ============================================= Binary Tree ==============================================
@@ -68,6 +69,42 @@ public:
         int left_height = maxDepth(root->left);
         int right_height = maxDepth(root->right);
         return 1 + max(left_height, right_height);
+    }
+};
+
+// 3
+class DiameterOfBT
+{
+    /*
+        We will compute the diameter in each node with bottom up approach,
+        the helper function returns the longest path which ends at root, which can be used by the parent node for diameter
+
+        *IMPORTANT*
+        We are computing number of nodes in diameter the problem wants edges, so it would be (no of nodes - 1)
+
+        T:O(n)
+        S:O(height) => O(n) worst case
+    */
+
+    int diameterOfBinaryTree_helper(TreeNode *root, int &diameter)
+    {
+        if (!root)
+            return 0;
+
+        auto left_path_length = diameterOfBinaryTree_helper(root->left, diameter);
+        auto right_path_length = diameterOfBinaryTree_helper(root->right, diameter);
+
+        diameter = max(diameter, left_path_length + right_path_length + 1);
+
+        return 1 + max(left_path_length, right_path_length);
+    }
+
+public:
+    int diameterOfBinaryTree(TreeNode *root)
+    {
+        int ans = 0;
+        diameterOfBinaryTree_helper(root, ans);
+        return ans == 0 ? 0 : ans - 1;
     }
 };
 // ============================================= Binary Search Tree ==============================================
