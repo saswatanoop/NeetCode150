0e06224f9853468f77d5cb11c8529b0942614314
sswt19 <saswatanoop@gmail.com>
2025-03-20 23:30:17 +0530

    subset 2

--- 9_Backtracking.py
+++ 9_Backtracking.py
@@ -1,4 +1,5 @@
 from typing import  List
+from collections import Counter
 
 # 1. https://leetcode.com/problems/subsets/
 def subsets(self, nums: List[int]) -> List[List[int]]:
@@ -17,6 +18,30 @@ def subsets(self, nums: List[int]) -> List[List[int]]:
     find_subset(0)
     return all_subsets
 
+# 2. https://leetcode.com/problems/subsets-ii/
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        # T:O(2^n) and S:O(n)
+        def find_subset(index):
+            if index==len(freq_list):
+                all_subsets.append(cur_subset[:])
+            else:
+                # ignore current index
+                find_subset(index+1)
+                # use current index
+                if freq_list[index][1]>0:
+                    freq_list[index][1]-=1
+                    cur_subset.append(freq_list[index][0])
+                    find_subset(index)
+                    freq_list[index][1]+=1
+                    cur_subset.pop()
+
+        freq_list=Counter(nums)
+        freq_list=[[k,v] for k,v in freq_list.items()]
+        cur_subset=[]
+        all_subsets=[]
+        find_subset(0)
+        return all_subsets
 # 9. https://leetcode.com/problems/letter-combinations-of-a-phone-number/
 def letterCombinations( digits: str) -> List[str]:
     # T:O(4^n) and S:O(n)
