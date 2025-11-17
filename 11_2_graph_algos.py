186dbd2ce1016f8d4128609b2f216b7a9b2e5d25
sswt19 <saswatanoop@gmail.com>
2025-11-17 06:21:14 +0530

    MST

--- 11_2_graph_algos.py
+++ 11_2_graph_algos.py
@@ -3,32 +3,69 @@
 
 '''
 MST:
-1. Krushal's Algorithm: DSU + greedy T:O(ElogE) S:O(V)
-2. Prim's Algorithm: Greedy + min-heap T:O(ElogV) S:O(V)
+1. Kruskal's Algorithm: DSU + greedy T:O(ElogE) S:O(V)
+2. Prim's Algorithm: Greedy + min-heap T:O(ElogV) S:O(V) as E=V^2 in dense graph, O(ElogE) = O(ElogV^2) = O(2ElogV) = O(ElogV)
 
 '''
 
 from additional_data_structures import DSU
+from collections import defaultdict
+import heapq
 
-def spanningTree(V, edges):
-    edges.sort(key=lambda x: x[2])
-    dsu=DSU(V)
-    edges_count=0
-    mst_sum=0
-    
-    for u,v,w in edges:
-        if dsu.find_parent(u)!=dsu.find_parent(v):
-            dsu.union(u,v)
-            edges_count+=1
-            mst_sum+=w
-            if edges_count==V-1:
+class MST:
+    def spanningTree_Kruskal(self, V, edges):
+        # 1. Kruskal's Algorithm: DSU + greedy T:O(ElogE) S:O(V)
+        edges.sort(key=lambda x: x[2])
+        dsu=DSU(V)
+        edges_count=0
+        mst_sum=0
+        
+        for u,v,w in edges:
+            if dsu.find_parent(u)!=dsu.find_parent(v):
+                dsu.union(u,v)
+                edges_count+=1
+                mst_sum+=w
+                # Early stopping
+                if edges_count==V-1:
+                    break
+        
+        # Very Important check for disconnected graph
+        if edges_count != V - 1:
+            return -1  # Graph is disconnected, MST not possible
+        
+        return mst_sum
+
+    def spanningTree_Prims(self, V, edges):
+        # 2. Prim's Algorithm: Greedy + min-heap T:O(ElogV) S:O(V) as E=V^2 = O(ElogV)
+        adjList=defaultdict(list)
+        for u,v,w in edges:
+            adjList[u].append((v,w))
+            adjList[v].append((u,w))
+            
+        mst_sum=0
+        min_heap=[(0,0,-1)] #weight, node, parent
+        visited=set()
+        
+        while min_heap:
+            weight,node,parent=heapq.heappop(min_heap)
+            
+            # Mark the visited only while poping from heap
+            if node not in visited:
+                visited.add(node)
+                mst_sum+=weight
+                # edge will be parent->node
+                
+                for nbr,w in adjList[node]:
+                    if nbr not in visited:
+                        heapq.heappush(min_heap,(w,nbr,node))
+            # Early stopping
+            if len(visited)==V:
                 break
-    
-    # Very Important check for disconnected graph
-    if edges_count != V - 1:
-        return -1  # Graph is disconnected, MST not possible
-    
-    return mst_sum
-                
-                
 
+        # Very Important check for disconnected graph
+        if len(visited)!=V:
+            return -1  # Graph is disconnected, MST not possible
+        
+        return mst_sum
+                    
+                
